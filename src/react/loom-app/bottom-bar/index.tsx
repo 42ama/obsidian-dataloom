9363ead019649b8942ef1025573843ee55666dad
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-12-01 01:13:36 -0700

    refactor: make bottom bar display as block

--- src/react/loom-app/bottom-bar/index.tsx
+++ src/react/loom-app/bottom-bar/index.tsx
@@ -29,53 +29,7 @@ export default function BottomBar({
 	onUndoClick,
 	onRedoClick,
 }: Props) {
-	const ref = React.useRef<HTMLDivElement | null>(null);
-	const [bottomBarOffset, setBottomBarOffset] = React.useState(0);
 	const isMobile = isOnMobile();
-
-	React.useEffect(() => {
-		const el = ref.current;
-		if (!el) return;
-
-		const appEl = el.closest(".dataloom-app");
-		if (!appEl) return;
-
-		const tableEl = appEl.querySelector(
-			".dataloom-table"
-		) as HTMLElement | null;
-		if (!tableEl) return;
-
-		const tableContainerEl = tableEl.parentElement;
-		if (!tableContainerEl) return;
-
-		function updateBottomBar(
-			tableEl: HTMLElement,
-			tableContainerEl: HTMLElement
-		) {
-			const tableRect = tableEl.getBoundingClientRect();
-			const tableContainerRect = tableContainerEl.getBoundingClientRect();
-
-			let diff = tableContainerRect.height - tableRect.height;
-			if (diff < 0) diff = 0;
-			setBottomBarOffset(diff);
-		}
-
-		const THROTTLE_TIME_MILLIS = 50;
-		const throttleUpdate = _.throttle(
-			updateBottomBar,
-			THROTTLE_TIME_MILLIS
-		);
-
-		const observer = new ResizeObserver(() => {
-			throttleUpdate(tableEl, tableContainerEl);
-		});
-
-		observer.observe(tableEl);
-
-		return () => {
-			observer.disconnect();
-		};
-	}, []);
 
 	let className = "dataloom-bottom-bar";
 	if (isMobile) {
@@ -83,48 +37,42 @@ export default function BottomBar({
 	}
 
 	return (
-		<div ref={ref} className={className}>
-			<div
-				style={{
-					top: numToPx(-bottomBarOffset),
-				}}
-			>
-				<Padding pt="md" width="100%">
-					<Stack spacing="sm">
+		<div className={className}>
+			<Padding py="sm" width="100%">
+				<Stack spacing="sm">
+					<Flex justify="space-between">
+						<NewRowButton onClick={onRowAddClick} />
+						<Stack isHorizontal spacing="sm">
+							<Button
+								ariaLabel="Scroll to top"
+								icon={<Icon lucideId="chevron-up" />}
+								onClick={onScrollToTopClick}
+							/>
+							<Button
+								ariaLabel="Scroll to bottom"
+								onClick={onScrollToBottomClick}
+								icon={<Icon lucideId="chevron-down" />}
+							/>
+						</Stack>
+					</Flex>
+					{isMobile && (
 						<Flex justify="space-between">
-							<NewRowButton onClick={onRowAddClick} />
-							<Stack isHorizontal spacing="sm">
-								<Button
-									ariaLabel="Scroll to top"
-									icon={<Icon lucideId="chevron-up" />}
-									onClick={onScrollToTopClick}
-								/>
-								<Button
-									ariaLabel="Scroll to bottom"
-									onClick={onScrollToBottomClick}
-									icon={<Icon lucideId="chevron-down" />}
-								/>
-							</Stack>
+							<Button
+								ariaLabel="Undo"
+								size="lg"
+								icon={<Icon lucideId="undo" />}
+								onClick={onUndoClick}
+							/>
+							<Button
+								ariaLabel="Redo"
+								size="lg"
+								icon={<Icon lucideId="redo" />}
+								onClick={onRedoClick}
+							/>
 						</Flex>
-						{isMobile && (
-							<Flex justify="space-between">
-								<Button
-									ariaLabel="Undo"
-									size="lg"
-									icon={<Icon lucideId="undo" />}
-									onClick={onUndoClick}
-								/>
-								<Button
-									ariaLabel="Redo"
-									size="lg"
-									icon={<Icon lucideId="redo" />}
-									onClick={onRedoClick}
-								/>
-							</Flex>
-						)}
-					</Stack>
-				</Padding>
-			</div>
+					)}
+				</Stack>
+			</Padding>
 		</div>
 	);
 }
